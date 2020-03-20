3f6422408889f5d9d8cf2a35e8bede94e5ae4955
mvdbeek <m.vandenbeek@gmail.com>
2020-03-20 21:39:58 +0100

    WIP: script for annotating accessions as Illumina

--- genomics/4-Variation/fetch_sra_acc.sh
+++ genomics/4-Variation/fetch_sra_acc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+set -e
 
 # From genbank
 curl -s https://www.ncbi.nlm.nih.gov/core/assets/genbank/files/ncov-sequences.yaml | grep -o "SRR[[:digit:]]\+" > genbank.txt
@@ -9,3 +10,9 @@ curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sra&amp;id
 
 cat genbank.txt sra.txt | sort | uniq > union.txt
 grep -f acc2exclude.txt -v union.txt > current.txt
+
+while read -r acc
+do
+    pysradb metadata "$acc" --saveto /dev/stdout >> current_metadata.txt || true
+done < current.txt
+grep Illumina current_metadata.txt |cut -f15 > current_illumina.txt
