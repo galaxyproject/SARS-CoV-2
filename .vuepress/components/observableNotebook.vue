60cc6630917ac9b42690f043f7cb6735542eeeff
Dannon Baker <dannon.baker@gmail.com>
2020-03-26 11:16:17 -0400

    Add link to notebook source

--- .vuepress/components/observableNotebook.vue
+++ .vuepress/components/observableNotebook.vue
@@ -1,5 +1,8 @@
 <template>
-    <div id="hotlinkedNotebookDock"></div>
+    <div>
+        <div id="hotlinkedNotebookDock"></div>
+        <p>Source notebook: <a :href="getNotebookSource">{{getNotebookSource}}</a></p>
+    </div>
 </template>
 
 <script>
@@ -10,9 +13,14 @@ export default {
             required: true
         }
     },
+    computed: {
+      getNotebookSource() {
+        return `https://observablehq.com/${this.notebookSource}`;
+      }
+    },
     mounted() {
         let notebookScript = document.createElement("script");
-        notebookScript.type = 'module';
+        notebookScript.type = "module";
         notebookScript.async = true;
         notebookScript.innerHTML = `
         import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
@@ -31,18 +39,18 @@ export default {
 <style>
 /* Default notebook styles, from https://raw.githubusercontent.com/observablehq/inspector/master/src/style.css */
 :root {
-  --syntax_normal: #1b1e23;
-  --syntax_comment: #a9b0bc;
-  --syntax_number: #20a5ba;
-  --syntax_keyword: #c30771;
-  --syntax_atom: #10a778;
-  --syntax_string: #008ec4;
-  --syntax_error: #ffbedc;
-  --syntax_unknown_variable: #838383;
-  --syntax_known_variable: #005f87;
-  --syntax_matchbracket: #20bbfc;
-  --syntax_key: #6636b4;
-  --mono_fonts: 82%/1.5 Menlo, Consolas, monospace;
+    --syntax_normal: #1b1e23;
+    --syntax_comment: #a9b0bc;
+    --syntax_number: #20a5ba;
+    --syntax_keyword: #c30771;
+    --syntax_atom: #10a778;
+    --syntax_string: #008ec4;
+    --syntax_error: #ffbedc;
+    --syntax_unknown_variable: #838383;
+    --syntax_known_variable: #005f87;
+    --syntax_matchbracket: #20bbfc;
+    --syntax_key: #6636b4;
+    --mono_fonts: 82%/1.5 Menlo, Consolas, monospace;
 }
 
 .observablehq--expanded,
@@ -52,41 +60,41 @@ export default {
 .observablehq--string:before,
 .observablehq--string:after,
 .observablehq--gray {
-  color: var(--syntax_normal);
+    color: var(--syntax_normal);
 }
 
 .observablehq--collapsed,
 .observablehq--inspect a {
-  cursor: pointer;
+    cursor: pointer;
 }
 
 .observablehq--field {
-  text-indent: -1em;
-  margin-left: 1em;
+    text-indent: -1em;
+    margin-left: 1em;
 }
 
 .observablehq--empty {
-  color: var(--syntax_comment);
+    color: var(--syntax_comment);
 }
 
 .observablehq--keyword,
 .observablehq--blue {
-  color: #3182bd;
+    color: #3182bd;
 }
 
 .observablehq--forbidden,
 .observablehq--pink {
-  color: #e377c2;
+    color: #e377c2;
 }
 
 .observablehq--orange {
-  color: #e6550d;
+    color: #e6550d;
 }
 
 .observablehq--null,
 .observablehq--undefined,
 .observablehq--boolean {
-  color: var(--syntax_atom);
+    color: var(--syntax_atom);
 }
 
 .observablehq--number,
@@ -95,41 +103,41 @@ export default {
 .observablehq--regexp,
 .observablehq--symbol,
 .observablehq--green {
-  color: var(--syntax_number);
+    color: var(--syntax_number);
 }
 
 .observablehq--index,
 .observablehq--key {
-  color: var(--syntax_key);
+    color: var(--syntax_key);
 }
 
 .observablehq--prototype-key {
-  color: #aaa;
+    color: #aaa;
 }
 
 .observablehq--empty {
-  font-style: oblique;
+    font-style: oblique;
 }
 
 .observablehq--string,
 .observablehq--purple {
-  color: var(--syntax_string);
+    color: var(--syntax_string);
 }
 
 .observablehq--error,
 .observablehq--red {
-  color: #e7040f;
+    color: #e7040f;
 }
 
 .observablehq--inspect {
-  font: var(--mono_fonts);
-  overflow-x: auto;
-  display: block;
-  white-space: pre;
+    font: var(--mono_fonts);
+    overflow-x: auto;
+    display: block;
+    white-space: pre;
 }
 
 .observablehq--error .observablehq--inspect {
-  word-break: break-all;
-  white-space: pre-wrap;
+    word-break: break-all;
+    white-space: pre-wrap;
 }
 </style>
