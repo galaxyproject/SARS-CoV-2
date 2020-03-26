9dc3548b0fd918101c95080057d695219214efbf
Dannon Baker <dannon.baker@gmail.com>
2020-03-26 17:03:31 -0400

    Add minimal jbrowse component/testbed so I can get other eyes on this inclusion issue...

---
+++ .vuepress/components/JBrowse.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Embedded JBrowse Component</h1>
+        <div class="jbrowse" id="GenomeBrowser">
+            <div id="LoadingScreen" style="padding: 50px;">
+                <h1>Loading...</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+//import "@gmod/jbrowse/dist/browser.bundle.js";
+export default {
+    mounted() {
+        let jbrowseBundle = document.createElement("script");
+        jbrowseBundle.type = "text/javascript";
+        jbrowseBundle.src = "/jbrowse/main.bundle.js";
+        document.head.appendChild(jbrowseBundle);
+    }
+};
+</script>
+<style>
+</style>
---
+++ genomics/jbrowse.md
@@ -0,0 +1,5 @@
+---
+title: Jbrowse?
+---
+
+<JBrowse/>
