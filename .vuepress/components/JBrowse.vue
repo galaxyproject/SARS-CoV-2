e507d45aeee6155940230571aa5d8855700bd834
Dannon Baker <dannon.baker@gmail.com>
2020-03-26 23:50:21 -0400

    Pivot to iframes...

--- .vuepress/components/JBrowse.vue
+++ .vuepress/components/JBrowse.vue
@@ -1,21 +1,20 @@
 <template>
     <div>
-        <h1>Embedded JBrowse Component</h1>
-        <div class="jbrowse" id="GenomeBrowser">
-            <div id="LoadingScreen" style="padding: 50px;">
-                <h1>Loading...</h1>
-            </div>
-        </div>
+        <h1>Test JBrowse Component</h1>
+          <iframe frameborder="0" height="600" width="960" src="/jbrowse/index.html"></iframe>
     </div>
 </template>
 <script>
 //import "@gmod/jbrowse/dist/browser.bundle.js";
 export default {
     mounted() {
+      console.debug("Mounted, should be an iframe up there somewhere!");
+      /*
         let jbrowseBundle = document.createElement("script");
         jbrowseBundle.type = "text/javascript";
         jbrowseBundle.src = "/jbrowse/browser.bundle.js";
         document.head.appendChild(jbrowseBundle);
+        */
     }
 };
 </script>
