20a205f24ce3a36f278c5e6c4d92b28f5813226e
Dannon Baker <dannon.baker@gmail.com>
2020-03-27 00:04:14 -0400

    Delegate control of config/loading to the parent vue component.  Iframe is literally just there to load jbrowse in a sandbox.

--- .vuepress/components/JBrowse.vue
+++ .vuepress/components/JBrowse.vue
@@ -1,22 +1,41 @@
 <template>
     <div>
         <h1>Test JBrowse Component</h1>
-          <iframe frameborder="0" height="600" width="960" src="/jbrowse/index.html"></iframe>
+        <iframe ref="jbframe" @load="load" frameborder="0" height="600" width="960" src="/jbrowse/index.html"></iframe>
     </div>
 </template>
 <script>
-//import "@gmod/jbrowse/dist/browser.bundle.js";
 export default {
-    mounted() {
-      console.debug("Mounted, should be an iframe up there somewhere!");
-      /*
-        let jbrowseBundle = document.createElement("script");
-        jbrowseBundle.type = "text/javascript";
-        jbrowseBundle.src = "/jbrowse/browser.bundle.js";
-        document.head.appendChild(jbrowseBundle);
-        */
+    methods: {
+        load() {
+            const features = [];
+            // Add some features
+            const config = {
+                containerID: "GenomeBrowser",
+                baseUrl: "/jbrowse/",
+                refSeqs: {
+                    url:
+                        "https://s3.amazonaws.com/1000genomes/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa.fai"
+                },
+                tracks: [
+                    {
+                        key: "GRCH38 Reference Sequence",
+                        label: "GRCH38 Reference Sequence",
+                        urlTemplate:
+                            "https://s3.amazonaws.com/1000genomes/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa"
+                    },
+                    {
+                        key: "MyTrack",
+                        label: "MyTrack",
+                        storeClass: "JBrowse/Store/SeqFeature/FromConfig",
+                        features: features,
+                        type: "CanvasVariants"
+                    }
+                ]
+            };
+            this.$refs.jbframe.contentWindow.instantiateBrowserWithConfig(config);
+        }
     }
 };
 </script>
-<style>
-</style>
+<style></style>
