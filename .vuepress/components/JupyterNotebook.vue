a833fa064d7c521fc039f5708989272120ed994d
Dannon Baker <dannon.baker@gmail.com>
2020-04-08 12:34:54 -0400

    Refactoring to more vue-y

--- .vuepress/components/JupyterNotebook.vue
+++ .vuepress/components/JupyterNotebook.vue
@@ -2,11 +2,41 @@
     <div class="nb-notebook" v-html="notebookHTML"></div>
 </template>
 <script>
-import axios from "axios";
 import "!!script-loader!notebookjs";
-import "prismjs/prism";
+import "./ipython-style.styl";
 import "prismjs/themes/prism.css";
-import './ipython-style.styl'
+import Prism from "prismjs/prism";
+import axios from "axios";
+import ansi_up from "ansi_up";
+import markdown from "marked";
+
+function defineHighlighter(code, lang) {
+    if (typeof lang === "undefined") lang = "markup";
+
+    if (!Prism.languages.hasOwnProperty(lang)) {
+        try {
+            require("prismjs/components/prism-" + lang + ".js");
+        } catch (e) {
+            console.warn("** failed to load Prism lang: " + lang);
+            Prism.languages[lang] = false;
+        }
+    }
+
+    return Prism.languages[lang] ? Prism.highlight(code, Prism.languages[lang]) : code;
+}
+
+/* configure global 'nb' */
+nb.ansi_up = ansi_up;
+nb.markdown = markdown;
+
+nb.highlighter = (text, pre, code, lang) => {
+    var language = lang || "text";
+    pre.className = "language-" + language;
+    if (typeof code != "undefined") {
+        code.className = "language-" + language;
+    }
+    return defineHighlighter(text, language);
+};
 
 export default {
     data: function() {
@@ -22,40 +52,8 @@ export default {
     },
     mounted() {
         axios.get(this.notebookURL).then(r => {
-            const nbParser = this.configureNotebookjs();
-            this.notebookHTML = nbParser.parse(r.data).render().innerHTML;
+            this.notebookHTML = nb.parse(r.data).render().innerHTML;
         });
-    },
-    methods: {
-        configureNotebookjs(){
-            nb.ansi_up = require('ansi_up');
-            nb.markdown = require('marked');
-
-            nb.highlighter = ((text, pre, code, lang) => {
-                var language = lang || 'text';
-                pre.className = 'language-' + language;
-                if (typeof code != 'undefined') {
-                    code.className = 'language-' + language;
-                }
-                return this.defineHighlighter(text, language);
-            });
-            return nb;
-        },
-        defineHighlighter(code, lang) {
-            if (typeof lang === 'undefined') lang = 'markup';
-
-            if (!Prism.languages.hasOwnProperty(lang)) {
-                try {
-                    require('prismjs/components/prism-' + lang + '.js');
-                } catch (e) {
-                    console.warn('** failed to load Prism lang: ' + lang);
-                    Prism.languages[lang] = false;
-                }
-            }
-
-            return Prism.languages[lang] ? Prism.highlight(code, Prism.languages[lang]) : code;
-        }
     }
-
-}
+};
 </script>
