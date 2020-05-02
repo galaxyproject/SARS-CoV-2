7084a89f4d9c05ef55b646bc64019f9db92ab21d
mvdbeek <m.vandenbeek@gmail.com>
2020-05-02 11:07:44 +0200

    Update workflow

--- genomics/4-Variation/covid_genome.py
+++ genomics/4-Variation/covid_genome.py
@@ -9,7 +9,7 @@ from workflow2executable.invocations import InvocationMonitor
 log = logging.Logger(__name__)
 
 
-@click.command("Run Download and SE+PE Illumina Covid workflows workflow")
+@click.command("Run Download and SE+PE Illumina Covid Variation Workflow workflow")
 @click.option("--nc_45512_genbank_file", type=click.Path(exists=True))
 @click.option("--accessions_file", type=click.Path(exists=True))
 @click.option('-a', '--api_key', help="API key to use for running workflow")
@@ -17,14 +17,14 @@ log = logging.Logger(__name__)
 @click.option('-h', '--history_id', help="History ID that will contain workflow results")
 @click.option('-n', '--new_history_name', help="Create a new history with this name. Will not be used if history ID is provided.")
 @click.option('--publish/--private', default=False, help="Publish history?")
-def run_download_and_se_pe_illumina_covid_workflows(nc_45512_genbank_file, accessions_file, api_key, galaxy_url, history_id, new_history_name, publish):
+def run_download_and_se_pe_illumina_covid_variation_workflow(nc_45512_genbank_file, accessions_file, api_key, galaxy_url, history_id, new_history_name, publish):
     """
-    Run Download and SE+PE Illumina Covid workflows workflow
+    Run Download and SE+PE Illumina Covid Variation Workflow workflow
     """
     gi = bioblend.galaxy.GalaxyInstance(galaxy_url, api_key)
     if history_id is None:
        if new_history_name is None:
-           new_history_name = 'History for Download and SE+PE Illumina Covid workflows execution'
+           new_history_name = 'History for Download and SE+PE Illumina Covid Variation Workflow execution'
        history_id = gi.histories.create_history(name=new_history_name)['id']
     if publish:
         gi.histories.update_history(history_id, published=True, importable=True)
@@ -36,7 +36,7 @@ def run_download_and_se_pe_illumina_covid_workflows(nc_45512_genbank_file, acces
         log.info("Uploading dataset %s", dataset)
         r = gi.tools.upload_file(path=dataset, history_id=history_id, to_posix_lines=False)
         upload_paths[dataset] = {'src': 'hda', 'id': r['outputs'][0]['id']}
-    workflow_id = 'e4604641-8de5-43ff-ba15-0ddc6e98ba53'
+    workflow_id = '053805ad-0607-4c39-afb8-25183fb899fb'
     inputs = {
             'NC_45512 GenBank file': upload_paths.get(nc_45512_genbank_file, nc_45512_genbank_file),
             'Accessions file': upload_paths.get(accessions_file, accessions_file),
@@ -57,4 +57,4 @@ def run_workflow(gi, workflow_id, history_id, inputs):
 
 
 if __name__ == '__main__':
-    run_download_and_se_pe_illumina_covid_workflows()
+    run_download_and_se_pe_illumina_covid_variation_workflow()
