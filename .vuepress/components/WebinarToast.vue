9771ecc1d3c92836762b1dabc73dee6627816c8c
Dannon Baker <dannon.baker@gmail.com>
2020-05-10 15:49:13 -0400

    Wrap toast in clientonly component to fix SSR (the build)

---
+++ .vuepress/components/WebinarToast.vue
@@ -0,0 +1,24 @@
+<template>
+    Nothing!
+</template>
+<script>
+import Toasted from "vue-toasted";
+import Vue from "vue";
+Vue.use(Toasted, { keepOnHover: true });
+
+export default {
+    mounted() {
+        this.$toasted.show("Join our COVID-19 webinars and learn about data analysis in Galaxy.", {
+            theme: "toasted-primary",
+            duration: 5000,
+            action: {
+                text: "open link",
+                onClick: (e, toastObject) => {
+                    window.open("https://elixir-europe.org/events/webinar-galaxy-elixir-covid19");
+                    toastObject.goAway(0);
+                }
+            }
+        });
+    }
+};
+</script>
--- .vuepress/theme/components/Navbar.vue
+++ .vuepress/theme/components/Navbar.vue
@@ -1,109 +1,95 @@
 <template>
-  <header class="navbar">
-    <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')" />
+    <header class="navbar">
+        <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')" />
 
-    <RouterLink
-      :to="$localePath"
-      class="home-link"
-    >
-      <img
-        v-if="$site.themeConfig.logo"
-        class="logo"
-        :src="$withBase($site.themeConfig.logo)"
-        :alt="$siteTitle"
-      >
-      <span
-        v-if="$siteTitle"
-        ref="siteName"
-        class="site-name"
-        :class="{ 'can-hide': $site.themeConfig.logo }"
-      >{{ $siteTitle }}</span>
-    </RouterLink>
+        <RouterLink :to="$localePath" class="home-link">
+            <img
+                v-if="$site.themeConfig.logo"
+                class="logo"
+                :src="$withBase($site.themeConfig.logo)"
+                :alt="$siteTitle"
+            />
+            <span v-if="$siteTitle" ref="siteName" class="site-name" :class="{ 'can-hide': $site.themeConfig.logo }">{{
+                $siteTitle
+            }}</span>
+        </RouterLink>
 
-    <div
-      class="links"
-      :style="linksWrapMaxWidth ? {
-        'max-width': linksWrapMaxWidth + 'px'
-      } : {}"
-    >
-      <AlgoliaSearchBox
-        v-if="isAlgoliaSearch"
-        :options="algolia"
-      />
-      <SearchBox v-else-if="$site.themeConfig.search !== false && $page.frontmatter.search !== false" />
-      <NavLinks class="can-hide" />
-    </div>
-  </header>
+        <div
+            class="links"
+            :style="
+                linksWrapMaxWidth
+                    ? {
+                          'max-width': linksWrapMaxWidth + 'px'
+                      }
+                    : {}
+            "
+        >
+            <AlgoliaSearchBox v-if="isAlgoliaSearch" :options="algolia" />
+            <SearchBox v-else-if="$site.themeConfig.search !== false && $page.frontmatter.search !== false" />
+            <NavLinks class="can-hide" />
+        </div>
+        <ClientOnly>
+            <WebinarToast />
+        </ClientOnly>
+    </header>
 </template>
 
 <script>
-import AlgoliaSearchBox from '@AlgoliaSearchBox'
-import SearchBox from '@SearchBox'
-import SidebarButton from '@theme/components/SidebarButton.vue'
-import NavLinks from '@theme/components/NavLinks.vue'
-import Toasted from 'vue-toasted';
-import Vue from 'vue'
-Vue.use(Toasted, {keepOnHover: true})
+import AlgoliaSearchBox from "@AlgoliaSearchBox";
+import SearchBox from "@SearchBox";
+import SidebarButton from "@theme/components/SidebarButton.vue";
+import NavLinks from "@theme/components/NavLinks.vue";
 
 export default {
-  name: 'Navbar',
+    name: "Navbar",
 
-  components: {
-    SidebarButton,
-    NavLinks,
-    SearchBox,
-    AlgoliaSearchBox
-  },
-
-  data () {
-    return {
-      linksWrapMaxWidth: null
-    }
-  },
-
-  computed: {
-    algolia () {
-      return this.$themeLocaleConfig.algolia || this.$site.themeConfig.algolia || {}
+    components: {
+        SidebarButton,
+        NavLinks,
+        SearchBox,
+        AlgoliaSearchBox
     },
 
-    isAlgoliaSearch () {
-      return this.algolia && this.algolia.apiKey && this.algolia.indexName
-    }
-  },
+    data() {
+        return {
+            linksWrapMaxWidth: null
+        };
+    },
 
-  mounted () {
-    const MOBILE_DESKTOP_BREAKPOINT = 719 // refer to config.styl
-    const NAVBAR_VERTICAL_PADDING = parseInt(css(this.$el, 'paddingLeft')) + parseInt(css(this.$el, 'paddingRight'))
-    const handleLinksWrapWidth = () => {
-      if (document.documentElement.clientWidth < MOBILE_DESKTOP_BREAKPOINT) {
-        this.linksWrapMaxWidth = null
-      } else {
-        this.linksWrapMaxWidth = this.$el.offsetWidth - NAVBAR_VERTICAL_PADDING
-          - (this.$refs.siteName && this.$refs.siteName.offsetWidth || 0)
-      }
-    }
-    handleLinksWrapWidth()
-    window.addEventListener('resize', handleLinksWrapWidth, false)
+    computed: {
+        algolia() {
+            return this.$themeLocaleConfig.algolia || this.$site.themeConfig.algolia || {};
+        },
 
-    this.$toasted.show("Join our COVID-19 webinars and learn about data analysis in Galaxy.", {
-      theme: "toasted-primary",
-      duration: 5000,
-      action: {
-        text: 'open link',
-        onClick: (e, toastObject) => {
-          window.open("https://elixir-europe.org/events/webinar-galaxy-elixir-covid19");
-          toastObject.goAway(0);
+        isAlgoliaSearch() {
+            return this.algolia && this.algolia.apiKey && this.algolia.indexName;
         }
-      },
-    })
-  }
-}
+    },
 
-function css (el, property) {
-  // NOTE: Known bug, will return 'auto' if style value is 'auto'
-  const win = el.ownerDocument.defaultView
-  // null means not to return pseudo styles
-  return win.getComputedStyle(el, null)[property]
+    mounted() {
+        const MOBILE_DESKTOP_BREAKPOINT = 719; // refer to config.styl
+        const NAVBAR_VERTICAL_PADDING =
+            parseInt(css(this.$el, "paddingLeft")) + parseInt(css(this.$el, "paddingRight"));
+        const handleLinksWrapWidth = () => {
+            if (document.documentElement.clientWidth < MOBILE_DESKTOP_BREAKPOINT) {
+                this.linksWrapMaxWidth = null;
+            } else {
+                this.linksWrapMaxWidth =
+                    this.$el.offsetWidth -
+                    NAVBAR_VERTICAL_PADDING -
+                    ((this.$refs.siteName && this.$refs.siteName.offsetWidth) || 0);
+            }
+        };
+        handleLinksWrapWidth();
+        window.addEventListener("resize", handleLinksWrapWidth, false);
+    }
+};
+
+function css(el, property) {
+    // NOTE: Known bug, will return 'auto' if style value is 'auto'
+    const win = el.ownerDocument.defaultView;
+    // null means not to return pseudo styles
+    return win.getComputedStyle(el, null)[property];
 }
 </script>
 
