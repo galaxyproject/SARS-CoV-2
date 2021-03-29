40d8d2575dc4e3672032657f05c09538b9917973
Dannon Baker <dannon.baker@gmail.com>
2021-03-29 11:43:30 -0400

    Include content from Anton

--- .vuepress/components/HomeFeatures.vue
+++ .vuepress/components/HomeFeatures.vue
@@ -22,8 +22,7 @@
                     <div class="h-feature-content">
                         <h3>PLoS Pathogens 2020</h3>
                         <p>
-                            No more business as usual: Agile and effective responses to emerging pathogen threats
-                            require open data and open analytics
+                            No more business as usual - Agile and effective responses to emerging pathogen threats require open data and open analytics
                         </p>
                     </div>
                     <div class="h-feature-focus">
@@ -36,7 +35,7 @@
                 <div class="h-feature">
                     <div class="h-feature-content">
                         <h3>Evolution</h3>
-                        <p>Natural Selection Analysis and Visualizations</p>
+                        <p>The SARS-CoV-2 Selection Pressure Analyses</p>
                     </div>
                     <div class="h-feature-focus">
                         <img :src="$withBase('./img/evo.png')" />
@@ -44,11 +43,36 @@
                 </div>
             </a>
 
+
+            <RouterLink :to="'/artic/'">
+                <div class="h-feature">
+                    <div class="h-feature-content">
+                        <h3>ARTIC</h3>
+                        <p>Amplicon based data analysis</p>
+                    </div>
+                    <div class="h-feature-focus">
+                        <img :src="$withBase('../img/artic.png')" />
+                    </div>
+                </div>
+            </RouterLink>
+
+            <RouterLink :to="'/direct-rnaseq/'">
+                <div class="h-feature">
+                    <div class="h-feature-content">
+                        <h3>Direct RNA seq</h3>
+                        <p>Analysis of SARS-CoV-2 transcription using Oxford Nanopore direct RNA sequencing.</p>
+                    </div>
+                    <div class="h-feature-focus">
+                        <img :src="$withBase('./img/drna.png')" />
+                    </div>
+                </div>
+            </RouterLink>
+
             <RouterLink :to="'/cheminformatics/'">
                 <h2>Cheminformatics</h2>
                 <div class="h-feature">
                     <div class="h-feature-content">
-                        <h4>Screening of the main protease</h4>
+                        <p>Docking simulations on the SARS-CoV-2 main protease. A Galaxy workflow for performing and evaluating molecular docking on a massive scale, based on the set of fragment hits which are known experimentally to bind to the protein.</p>
                     </div>
                     <div class="h-feature-focus">
                         <img :src="$withBase('./img/mp.png')" />
@@ -61,7 +85,7 @@
                 <div class="h-feature">
                     <div class="h-feature-content">
                         <h4>Mass Spectrometry</h4>
-                        <p>More text about mass spec</p>
+                        <p>Galaxy workflows for analysis of Mass Spectrometry datasets from cell culture samples, clinical samples and protein-protein interaction datasets as well as functionality for performing metaproteomics analyses.</p>
                     </div>
                     <div class="h-feature-focus">
                         <img :src="$withBase('./img/pro.png')" />
