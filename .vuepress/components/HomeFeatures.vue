4a5b0ccef9130678f3825d05ed7e68113abd5614
Anton Nekrutenko <anton@Antons-MacBook-Air.local>
2021-03-29 17:05:35 -0400

    added text to homepage

--- .vuepress/components/HomeFeatures.vue
+++ .vuepress/components/HomeFeatures.vue
@@ -7,26 +7,16 @@
                 <div class="h-feature">
                     <div class="h-feature-content">
                         <h3>
-                            Global Platform<sup><font color="red">new</font></sup>
+                            Freely accessible ready to use global infrastructure for SARS-CoV-2 monitoring<sup><font color="red">new</font></sup> [<a href="https://doi.org/10.1101/2021.03.25.437046">bioRxiv</a>]
                         </h3>
-                        <p>Freely accessible ready to use global infrastructure for SARS-CoV-2 monitoring</p>
-                    </div>
-                    <div class="h-feature-focus">
-                        <img :src="$withBase('./genomics/global_platform/figs/fig2.png')" />
-                    </div>
-                </div>
-            </RouterLink>
-
-            <RouterLink :to="'/genomics/no-more-business-as-usual/'">
-                <div class="h-feature">
-                    <div class="h-feature-content">
-                        <h3>PLoS Pathogens 2020</h3>
                         <p>
-                            No more business as usual - Agile and effective responses to emerging pathogen threats require open data and open analytics
-                        </p>
+                        Our system is specifically designed to encourage collaborative worldwide genomic surveillance to rapidly identify and respond to emerging variants, its use of raw read data rather than assembled genomes goes a step beyond current surveillance efforts. Specifically it enables the coordinated worldwide surveillance of intra-patient minor AV frequencies: a distinction that could yield decisive early-warnings of epidemiological conditions conducive to the emergence of variants with altered pathogenicity, vaccine sensitivity or resistance. <br>
+                        <a href="https://doi.org/10.1101/2021.03.25.437046">BioRxiv Preprint</a>
+                    </p>
+
                     </div>
                     <div class="h-feature-focus">
-                        <img :src="$withBase('./img/gn.png')" />
+                        <img :src="$withBase('../img/gn.png')" />
                     </div>
                 </div>
             </RouterLink>
@@ -35,7 +25,7 @@
                 <div class="h-feature">
                     <div class="h-feature-content">
                         <h3>Evolution</h3>
-                        <p>The SARS-CoV-2 Selection Pressure Analyses</p>
+                        <p>The list of selected sites in SARS-CoV-2 genome. It is continuously updated by the <a href="http://datamonkey.org/">DataMonkey</a> team using GISAID data as it accumulates. This list included all codons identified with <a href="http://datamonkey.org/fel">FEL</a> and <a href="http://datamonkey.org/meme">MEME</a> methods to be evolving under positive or negative selection.</p>
                     </div>
                     <div class="h-feature-focus">
                         <img :src="$withBase('./img/evo.png')" />
@@ -46,8 +36,8 @@
             <RouterLink :to="'/artic/'">
                 <div class="h-feature">
                     <div class="h-feature-content">
-                        <h3>ARTIC</h3>
-                        <p>Amplicon based data analysis</p>
+                        <h3>ARTIC: Amplicon based data analysis</h3>
+                        <p>A series of state-of-the-art workflows for the analysis of SARS-CoV-2 ARTIC data using the Galaxy platform.</p>
                     </div>
                     <div class="h-feature-focus">
                         <img :src="$withBase('../img/artic.png')" />
@@ -58,11 +48,26 @@
             <RouterLink :to="'/direct-rnaseq/'">
                 <div class="h-feature">
                     <div class="h-feature-content">
-                        <h3>Direct RNA seq</h3>
-                        <p>Analysis of SARS-CoV-2 transcription using Oxford Nanopore direct RNA sequencing.</p>
+                        <h3>Analysis of SARS-CoV-2 transcription using Oxford Nanopore direct RNA sequencing [<a href="https://doi.org/10.1101/2020.07.18.204362">bioRxiv</a>]</h3>
+                        <p>We use direct RNA sequencing to precisely annotate the open reading frames and the landscape of SARS-CoV-2 RNA modifications. Conservation of the RNA modification pattern during progression of the current pandemic suggests that this pattern is likely essential for the life cycle of SARS-CoV-2 and represents a possible target for drug interventions. We also provide Galaxy workflows for the analysis of DRS data.
+</p>
                     </div>
                     <div class="h-feature-focus">
-                        <img :src="$withBase('./img/drna.png')" />
+                        <img :src="$withBase('./img/drs.png')" />
+                    </div>
+                </div>
+            </RouterLink>
+
+             <RouterLink :to="'/genomics/no-more-business-as-usual/'">
+                <div class="h-feature">
+                    <div class="h-feature-content">
+                        <h3>No more business as usual - Agile and effective responses to emerging pathogen threats require open data and open analytics [<a href="https://doi.org/10.1371/journal.ppat.1008643">PLoS Pathogens</a>]</h3>
+                        <p>
+                         In an age of digital connectedness, open, highly accessible, globally shared data and analysis platforms have the potential to transform the way biomedical research is done, opening the way to “global research markets” in which competition arises from deriving understanding rather than access to samples and data.    
+                        </p>
+                    </div>
+                    <div class="h-feature-focus">
+                        <img :src="$withBase('./img/plos_p.png')" />
                     </div>
                 </div>
             </RouterLink>
@@ -74,7 +79,7 @@
                         <p>Docking simulations on the SARS-CoV-2 main protease. A Galaxy workflow for performing and evaluating molecular docking on a massive scale, based on the set of fragment hits which are known experimentally to bind to the protein.</p>
                     </div>
                     <div class="h-feature-focus">
-                        <img :src="$withBase('./img/mp.png')" />
+                        <img :src="$withBase('./img/cheminfo.png')" />
                     </div>
                 </div>
             </RouterLink>
@@ -86,7 +91,7 @@
                         <p>Galaxy workflows for analysis of Mass Spectrometry datasets from cell culture samples, clinical samples and protein-protein interaction datasets as well as functionality for performing metaproteomics analyses.</p>
                     </div>
                     <div class="h-feature-focus">
-                        <img :src="$withBase('./img/pro.png')" />
+                        <img :src="$withBase('./img/mspec.png')" />
                     </div>
                 </div>
             </RouterLink>
