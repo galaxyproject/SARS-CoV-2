ac10fbe551bf58feed2d6361bf3d39d8181495e2
nekrut <anekrut@gmail.com>
2021-04-26 16:56:04 -0400

    rearranged genomics section and added extra info. Fixed button links

--- .vuepress/components/Monitoring.vue
+++ .vuepress/components/Monitoring.vue
@@ -3,16 +3,14 @@
         <h2 id="monitoring">
             SARS-CoV-2 Monitoring
         </h2>
+        <a href="https://observablehq.com/@spond/sars-cov-2-cog-uk"><img :src="'/genomics/global_platform/figs/dashboard.png'" /></a>
+        <hr>
         <div id="mon-tabs">
             <!-- These can be `a` or `RouterLink`, just apply the 'action-button' class -->
-            <RouterLink class="action-button" :to="'/genomics/global_platform/'">
-                Approach
-            </RouterLink>
-            <a href="https://covid19beacon.crg.eu/" class="action-button">Data</a>
-            <a href="https://observablehq.com/@spond/intrahost-variant-exploration-landing" class="action-button"
-                >Dashboard</a
-            >
-            <a class="action-button">How To?</a>
+            <RouterLink class="action-button" :to="'/genomics/global_platform/'">Why?</RouterLink>
+            <RouterLink class="action-button" :to="'/genomics/global_platform/#workflows-for-discovery-of-sequence-variants'">Workflows</RouterLink>
+            <RouterLink class="action-button" :to="'/genomics/global_platform/#processed-cog-uk-data'">Data</RouterLink>
+            <RouterLink class="action-button" :to="'/genomics/global_platform/#how-do-i-use-it'">How To?</RouterLink>
         </div>
     </div>
 </template>
@@ -37,20 +35,20 @@ export default {
         margin: 0 2rem;
         font-size: 1.2rem;
         color: #fff;
-        border: 1px solid lighten($accentColor, 10%);
-        background-color: lighten($accentColor, 10%);
+        border: 1px solid lighten($buttonColor, 10%);
+        background-color: lighten($buttonColor, 10%);
         padding: 0.8rem 1.6rem;
         border-radius: 4px;
         transition: background-color 0.2s ease;
         font-weight: 500;
         &:hover {
-            border: 1px solid $accentColor;
-            background-color: lighten($accentColor, 25%);
+            border: 1px solid $buttonColor;
+            background-color: lighten($buttonColor, 35%);
         }
         /*
-        // background-image: linear-gradient(to right, lighten($accentColor, 15%) 0%, $accentColor 51%, lighten($accentColor, 15%) 100%);
+        // background-image: linear-gradient(to right, lighten($buttonColor, 15%) 0%, $buttonColor 51%, lighten($buttonColor, 15%) 100%);
         &:hover {
-            background-image: linear-gradient(to right, lighten($accentColor, 35%) 0%, $accentColor 51%, lighten($accentColor, 35%) 100%);
+            background-image: linear-gradient(to right, lighten($buttonColor, 35%) 0%, $buttonColor 51%, lighten($buttonColor, 35%) 100%);
         */
     }
 }
