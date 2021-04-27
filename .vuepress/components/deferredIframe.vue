efc1f023495fb89a5aabb2c5a699e320be6d2601
Dannon Baker <dannon.baker@gmail.com>
2021-04-27 10:14:44 -0400

    Polish, use skeleton-style placeholder

--- .vuepress/components/deferredIframe.vue
+++ .vuepress/components/deferredIframe.vue
@@ -1,10 +1,13 @@
 <template>
-    <div v-if="loadNotebook">
-        <iframe width="100%" height="796" frameborder="0" :src="src"></iframe>
-    </div>
-    <div v-else="loadNotebook">
+    <div class="placeholderContainer">
+        <iframe v-if="loadFrame" :width="width" :height="height" frameborder="0" :src="src"></iframe>
         <!-- Shouldn't ever see this button unless the intersection API isn't available, this is a fallback -->
-        <a class="action-button" href="#" @click.prevent='loadMe'>Click here to load Observable frame</a>
+        <a v-else class="iframePlaceholder" href="#" @click.prevent="loadMe">
+            <h3>
+                Click here to load iframe
+            </h3>
+            <h5>{{ src }}</h5>
+        </a>
     </div>
 </template>
 
@@ -14,11 +17,19 @@ export default {
         src: {
             type: String,
             required: true
+        },
+        width: {
+            type: String,
+            default: "100%"
+        },
+        height: {
+            type: String,
+            default: "796"
         }
     },
     data() {
         return {
-            loadNotebook: false
+            loadFrame: false
         };
     },
     mounted() {
@@ -29,28 +40,21 @@ export default {
     },
     methods: {
         loadMe() {
-            this.loadNotebook = true;
-        },
+            this.loadFrame = true;
+        }
     }
 };
 </script>
-<style lang="stylus">
-    // TODO: a spinner or something other than a button, and refactor these styles to be generally applicable.
-    .action-button {
-        flex: 1 1 0px
-        text-align: center;
-        margin: 0 2rem;
-        font-size: 1.2rem;
-        color: #fff;
-        border: 1px solid lighten($buttonColor, 10%);
-        background-color: lighten($buttonColor, 10%);
-        padding: 0.8rem 1.6rem;
-        border-radius: 4px;
-        transition: background-color 0.2s ease;
-        font-weight: 500;
-        &:hover {
-            border: 1px solid $buttonColor;
-            background-color: lighten($buttonColor, 35%);
-        }
-    }
+<style>
+.placeholderContainer {
+    width: 100%;
+}
+.iframePlaceholder {
+    display: block;
+    width: 100%;
+    height: 796px;
+    background: #ccc;
+    text-align: center;
+    padding-top: 5rem;
+}
 </style>
